832bd73be03903f9b53c60e1768f8173160de872
Juan Pablo Holguin <juanpaholguin15@gmail.com>
2023-12-21 16:17:26 -0500

    Finalizado

--- App.js/App.js
+++ App.js/App.js
@@ -1,127 +1,182 @@
-const bar = document.getElementById('bar');
+const bar = document.getElementById("bar");
 
-const nav =  document.getElementById('navbar');
+const nav = document.getElementById("navbar");
 
-const close = document.getElementById('close');
+const close = document.getElementById("close");
 
-const closeCountp = document.querySelector('.count-products');
+const closeCountp = document.querySelector(".count-products");
 
-if (bar){
-    bar.addEventListener('click', ()=>{
-        nav.classList.add('active');
-		closeCountp.classList.add('hidden');
-    })
+const listadoproductos = document.querySelector("#productos");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+    closeCountp.classList.add("hidden");
+  });
 }
 
-if (close){
-    close.addEventListener('click', ()=>{
-        nav.classList.remove('active');
-		closeCountp.classList.remove('hidden');
-    })
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+    closeCountp.classList.remove("hidden");
+  });
+}
+/*Cargar Productos con Promesa y Apis*/
+/* Haciendo llamado de api*/
+
+
+
+
+function recuperarp() {
+  return new Promise((resolve, reject) => {
+    fetch("../JSON/productos.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al cargar la Api");
+        }
+
+        return response.json();
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
 }
 
+recuperarp("../JSON/productos.json")
+  .then((data) => {
+    main(data);
+  })
+  .catch((error) => {
+    // Manejar errores
+    console.error("Error al obtener datos de la API:", error);
+  });
 
-const btnCart = document.querySelector('.container-cart-icon');
+async function main() {
+  try {
+    const mostrarproductos = await recuperarp();
+
+    mostrarproductos.forEach((producto) => {
+      const div = document.createElement("div");
+      div.classList.add("pro");
+      div.innerHTML = ` 
+                    <img src="${producto.imagen}" alt="">
+                    <div class="des">
+                        <span>${producto.marca}</span>
+                        <h5>${producto.nombre}</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4 class="price">$${producto.precio}</h4>
+                        <button class="normal añadircarrito">Añadir Carrito</button>
+                    </div>
+                    <a href="#"><i class="fal fa-shopping-cart cart añadircarrito" ></i></a>
+                    `;
+      listadoproductos.append(div);
+    });
+  } catch (error) {
+    console.error("el error es:", error);
+  }
+}
+const btnCart = document.querySelector(".container-cart-icon");
 const containerCartProducts = document.querySelector(
-	'.container-cart-products'
+  ".container-cart-products"
 );
 
-btnCart.addEventListener('click', () => {
-	containerCartProducts.classList.toggle('hidden-cart');
+btnCart.addEventListener("click", () => {
+  containerCartProducts.classList.toggle("hidden-cart");
 });
 
-
-/*Nuevo codigo*/ 
-const cartInfo = document.querySelector('.cart-product');
-const rowProduct = document.querySelector('.row-product');
+/*Nuevo codigo*/
+const cartInfo = document.querySelector(".cart-product");
+const rowProduct = document.querySelector(".row-product");
 
 // Lista de todos los contenedores de productos
-const productsList = document.querySelector('.pro-container');
+const productsList = document.querySelector(".pro-container");
 
 // Variable de arreglos de Productos
 let allProducts = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const valorTotal = document.querySelector('.total-pagar');
+const valorTotal = document.querySelector(".total-pagar");
 
-const countProducts = document.querySelector('#contador-productos');
+const countProducts = document.querySelector("#contador-productos");
 
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
+const cartEmpty = document.querySelector(".cart-empty");
+const cartTotal = document.querySelector(".cart-total");
 
-productsList.addEventListener('click', e => {
-	if (e.target.classList.contains('añadircarrito')) {
-		const product = e.target.parentElement;
-		
+productsList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("añadircarrito")) {
+    const product = e.target.parentElement;
 
-		const infoProduct = {
-			quantity: 1,
-			title: product.querySelector('h5').textContent,
-			price: product.querySelector('h4').textContent,
-		};
+    const infoProduct = {
+      quantity: 1,
+      title: product.querySelector("h5").textContent,
+      price: product.querySelector("h4").textContent,
+    };
 
-		const exits = allProducts.some(
-			product => product.title === infoProduct.title
-		);
+    const exits = allProducts.some(
+      (product) => product.title === infoProduct.title
+    );
 
-		if (exits) {
-			const products = allProducts.map(product => {
-				if (product.title === infoProduct.title) {
-					product.quantity++;
-					return product;
-				} else {
-					return product;
-				}
-			});
-			allProducts = [...products];
-		} else {
-			allProducts = [...allProducts, infoProduct];
-		}
+    if (exits) {
+      const products = allProducts.map((product) => {
+        if (product.title === infoProduct.title) {
+          product.quantity++;
+          return product;
+        } else {
+          return product;
+        }
+      });
+      allProducts = [...products];
+    } else {
+      allProducts = [...allProducts, infoProduct];
+    }
 
-		showHTML();
-		saveLocal();
-	}
+    showHTML();
+    saveLocal();
+  }
 });
 
-rowProduct.addEventListener('click', e => {
-	if (e.target.classList.contains('icon-close')) {
-		const product = e.target.parentElement;
-		const title = product.querySelector('p').textContent;
-		localStorage.clear();
+rowProduct.addEventListener("click", (e) => {
+  if (e.target.classList.contains("icon-close")) {
+    const product = e.target.parentElement;
+    const title = product.querySelector("p").textContent;
+    localStorage.clear();
 
-		allProducts = allProducts.filter(
-			product => product.title !== title,
-			
-		);
+    allProducts = allProducts.filter((product) => product.title !== title);
 
-		console.log(allProducts);
+    console.log(allProducts);
 
-		showHTML();
-	}
+    showHTML();
+  }
 });
 
 // Funcion para mostrar  HTML
 const showHTML = () => {
-	if (!allProducts.length) {
-		cartEmpty.classList.remove('hidden');
-		rowProduct.classList.add('hidden');
-		cartTotal.classList.add('hidden');
-	} else {
-		cartEmpty.classList.add('hidden');
-		rowProduct.classList.remove('hidden');
-		cartTotal.classList.remove('hidden');
-	}
+  if (!allProducts.length) {
+    cartEmpty.classList.remove("hidden");
+    rowProduct.classList.add("hidden");
+    cartTotal.classList.add("hidden");
+  } else {
+    cartEmpty.classList.add("hidden");
+    rowProduct.classList.remove("hidden");
+    cartTotal.classList.remove("hidden");
+  }
 
-	// Limpiar HTML
-	rowProduct.innerHTML = '';
+  // Limpiar HTML
+  rowProduct.innerHTML = "";
 
-	let total = 0;
-	let totalOfProducts = 0;
+  let total = 0;
+  let totalOfProducts = 0;
 
-	allProducts.forEach(product => {
-		const containerProduct = document.createElement('div');
-		containerProduct.classList.add('cart-product');
+  allProducts.forEach((product) => {
+    const containerProduct = document.createElement("div");
+    containerProduct.classList.add("cart-product");
 
-		containerProduct.innerHTML = `
+    containerProduct.innerHTML = `
             <div class="info-cart-product">
                 <span class="cantidad-producto-carrito">${product.quantity}</span>
                 <p class="titulo-producto-carrito">${product.title}</p>
@@ -143,55 +198,23 @@ const showHTML = () => {
             </svg>
         `;
 
-		rowProduct.append(containerProduct);
+    rowProduct.append(containerProduct);
 
-		total =
-			total + parseInt(product.quantity * product.price.slice(1));
-		totalOfProducts = totalOfProducts + product.quantity;
-		localStorage.setItem("contadorp", JSON.stringify(totalOfProducts))
-	});
+    total = total + parseInt(product.quantity * product.price.slice(1));
+    totalOfProducts = totalOfProducts + product.quantity;
+    localStorage.setItem("contadorp", JSON.stringify(totalOfProducts));
+  });
 
-	valorTotal.innerText = `$${total}`;
-	countProducts.innerText = JSON.parse(localStorage.getItem("contadorp"));
+  valorTotal.innerText = `$${total}`;
+  countProducts.innerText = JSON.parse(localStorage.getItem("contadorp"));
 };
-
 
 const saveLocal = () => {
-	localStorage.setItem("carrito", JSON.stringify(allProducts));
+  localStorage.setItem("carrito", JSON.stringify(allProducts));
 };
 
-
-window.addEventListener('load', () => {
-    allProducts = JSON.parse(localStorage.getItem("carrito")) || [];
-    showHTML();
+window.addEventListener("load", () => {
+  allProducts = JSON.parse(localStorage.getItem("carrito")) || [];
+  showHTML();
 });
 
-
-
-function recuperarp(){
-    return new Promise((resolve, reject) => {
-        fetch('../JSON/productos.json')
-        .then(response => {
-            if(!response.ok){
-                throw new Error("Error al cargar la Api");
-            }
-
-            return recuperarp();
-        })
-        .then(data => resolve(data))
-        .catch(error => reject(error));
-    });
-};
-
-
-async function main(){
-    try{
-        const informacionp = await recuperarp()
-        console.log(informacionp)
-    }catch(error){
-        console.error("Surgio un error:", error)
-
-    }
-}
-
-main();
